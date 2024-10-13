2da05b62a6dd3ec566d3abbaf0fd32104037abfe
Yuval <yuval@khaskelberg.org>
2024-10-13 13:41:12 +0300

    fix

--- Code/empty_strategic.py
+++ Code/empty_strategic.py
@@ -11,7 +11,7 @@ ENEMY_TILE = 2
 
 builder_built_builder = {}
 
-def mass_center_of_our_territory(strategic: StrategicApi):
+def mass_center_of_our_territory(strategic: StrategicApi) -> Coordinates:
     our_tiles = []
     x_sum = 0
     y_sum = 0
@@ -33,7 +33,7 @@ def mass_center_of_our_territory(strategic: StrategicApi):
 
     
 
-def get_ring_of_radius(strategic: StrategicApi, tile: Tile, r: int):
+def get_ring_of_radius(strategic: StrategicApi, tile: Tile, r: int) -> list[Coordinates]:
     ret = []
     x, y = tile.coordinates.x, tile.coordinates.y
     for i in range(-r, r+1):
@@ -44,9 +44,9 @@ def get_ring_of_radius(strategic: StrategicApi, tile: Tile, r: int):
     return ret
 
 
-def get_tile_to_attack(strategic: StrategicApi, center: Coordinates, tank_tile: Tile) -> Tile:
+def get_tile_to_attack(strategic: StrategicApi, center: Coordinates, tank_tile: Tile) -> Coordinates:
     radius = 1
-    possible_tiles = []
+    possible_tiles: list[Coordinates] = []
     while True:
         if radius >= 100:
             return None # everything is ours, long live Shahaf the king
@@ -69,7 +69,7 @@ def do_turn(strategic: StrategicApi):
     for piece, command_id in attacking_pieces.items():
         if command_id is not None:
             continue
-        strategic.attack({StrategicPiece(piece.id, piece.type)}, get_tile_to_attack(strategic, mass_center_of_our_territory(strategic), piece.tile).coordinates, 1)
+        strategic.attack({StrategicPiece(piece.id, piece.type)}, get_tile_to_attack(strategic, mass_center_of_our_territory(strategic), piece.tile), 1)
 
     builders : dict = strategic.report_builders()
 

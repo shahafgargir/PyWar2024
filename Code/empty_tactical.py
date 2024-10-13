d9ffc5bfe678f64d1c2ff0c79eee097d54c02830
Yuval <yuval@khaskelberg.org>
2024-10-13 14:47:55 +0300

    fix

--- Code/empty_tactical.py
+++ Code/empty_tactical.py
@@ -127,7 +127,6 @@ def builder_do_work(context: TurnContext, builder: Builder, piece_type: str):
             elif piece_type == 'builder':
                 builder.build_builder()
             builder_to_building_command[builder.id] = CommandStatus.success(command_id)
-            del builder_to_piece_type[builder.id]
             return True
     # we dont have enough money, go collect it!
     return builder_collect_money(context, builder)
